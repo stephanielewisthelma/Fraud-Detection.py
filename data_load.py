0be6d88cf279a3b067b6836f29608613cbf2764c
stephanielewisthelma <slewisidc230004@gmail.com>
2025-09-26 05:11:02 +0100

    data load and missed values

--- data_load.py
+++ data_load.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
-# Load dataset
+
 df = pd.read_csv("bank_transactions.csv")
 
-# Look at the first 5 rows
+
 print(df.head())
 
-# Check rows and columns
 print("Shape of dataset:", df.shape)
 
-# Info about data types
 print(df.info())
 
-# Basic statistics
 print(df.describe())
+
+#  Missing values
+print(df.isnull().sum())
+
+print("Duplicates:", df.duplicated().sum())
+
+
+df = df.drop_duplicates()
