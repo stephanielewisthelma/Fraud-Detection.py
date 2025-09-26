ff8dcf0ab51087302744377990b6f8c5d46854a8
stephanielewisthelma <slewisidc230004@gmail.com>
2025-09-26 05:13:39 +0100

    EDA

---
+++ EDA.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv("bank_transactions.csv")
+
+
+
+# Fraud vs Non-Fraud
+print(df["is_fraud"].value_counts())
+sns.countplot(x="is_fraud", data=df)
+plt.title("Fraud vs Non-Fraud")
+plt.show()
+
+# Amount distribution
+if "amount" in df.columns:
+    sns.histplot(df["amount"], bins=50)
+    plt.title("Transaction Amount Distribution")
+    plt.show()
+
+    sns.boxplot(x="is_fraud", y="amount", data=df)
+    plt.title("Amount by Fraud/Non-Fraud")
+    plt.show()
+
+# Correlation heatmap
+corr = df.corr(numeric_only=True)
+plt.figure(figsize=(10,6))
+sns.heatmap(corr, cmap="coolwarm")
+plt.title("Correlation Heatmap")
+plt.show()
